71b05bdc7c78f080a220f098f531b83cab995069
Wonik <wonik.suh@outlook.com>
2026-01-31 19:36:57 -0800

    feat: trigger user-approved screen capture from Freeze button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Freeze v1 loaded");
 
 const openButton = document.getElementById("openBtn");
 const urlInput = document.getElementById("urlInput");
+const freezeButton = document.getElementById("freezeBtn")
 
 openButton.addEventListener("click", () => {
   const url = urlInput.value;
@@ -11,4 +12,18 @@ openButton.addEventListener("click", () => {
   }
 
   window.open(url, "_blank");
+});
+
+freezeButton.addEventListener("click", async () => {
+    try {
+        const stream = await navigator.mediaDevices.getDisplayMedia({
+            video: true
+        });
+
+        stream.getTracks().forEach((track) => track.stop());
+
+        console.log("Capture stream received and stopped");
+    } catch (err) {
+        console.log("Capture cancelled or blocked");
+    }
 });
