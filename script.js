bda942f47ef2631d06a920c70f7cd5672e07d658
Wonik <wonik.suh@outlook.com>
2026-01-31 19:54:18 -0800

    feat: capture single frame and display frozen image

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Freeze v1 loaded");
 const openButton = document.getElementById("openBtn");
 const urlInput = document.getElementById("urlInput");
 const freezeButton = document.getElementById("freezeBtn")
+const freezeImage = document.getElementById("freezeImage")
 
 openButton.addEventListener("click", () => {
   const url = urlInput.value;
@@ -20,9 +21,21 @@ freezeButton.addEventListener("click", async () => {
             video: true
         });
 
-        stream.getTracks().forEach((track) => track.stop());
+        const video = document.createElement("video");
+        video.srcObject = stream;
 
-        console.log("Capture stream received and stopped");
+        await video.play();
+
+        const canvas = document.createElement("canvas");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        freezeImage.src = canvas.toDataURL("image/png");
+
+        stream.getTracks().forEach((track) => track.stop());
     } catch (err) {
         console.log("Capture cancelled or blocked");
     }
